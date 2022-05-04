be7be33a7e477595f2e5a83ff816fa6ac4e0c191
204 <204@academicos.cesde.edu.co>
2022-05-04 07:00:45 -0500

    registros

---
+++ js/controladoregister.js
@@ -0,0 +1,15 @@
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    console.log("Usuario adentro "+user.email)
+    
+  } else {
+    console.log("Usuario afuera")
+
+  }
+});
--- js/register.js
+++ js/register.js
@@ -1,8 +1,10 @@
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 let boton = document.getElementById("button")
+let modalregistro = new bootstrap.Modal(document.getElementById('mensaje'))
+let textomodal = document.getElementById("mensajemodal")
 
-boton.addEventListener("click",function(evento){
+boton.addEventListener("click", function (evento) {
     evento.preventDefault()
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
@@ -13,11 +15,25 @@ boton.addEventListener("click",function(evento){
             // Signed in
             const user = userCredential.user;
             console.log("Exito en el registro")
+            textomodal.textContent = "Exito en el registro"
+            modalregistro.show()
+            let formulario = document.getElementById("formregister")
+            formulario.reset()
+            
+            setTimeout(function () {
+                modalregistro.hide()
+            }, 4000)
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            console.log("fallo"+errorMessage)
+            console.log("fallo" + errorMessage)
+            textomodal.textContent = "Fallaamos gente :( " + errorMessage
+            modalregistro.show()
+
+            setTimeout(function () {
+                modalregistro.hide()
+            }, 4000)
             // ..
         });
 
